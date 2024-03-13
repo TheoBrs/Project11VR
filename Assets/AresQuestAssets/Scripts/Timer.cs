fc0aecf0a9556dc69ef1ab78df06e45687ccd8fe
TheoBrs <tboros@gaming.tech>
2024-03-13 12:14:17 +0100

    Functionnal timer prefab

---
+++ Assets/AresQuestAssets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text minutes;
+    public TMP_Text seconds;
+
+    public float totalTime = 75;
+
+    void Update()
+    {
+        if (totalTime > 0)
+        {
+            totalTime -= Time.deltaTime;
+        }
+        else if(totalTime == 0)
+        {
+            //Player lost
+        }
+
+        float min = Mathf.FloorToInt(totalTime / 60);
+        float sec = Mathf.FloorToInt(totalTime % 60);
+
+        double b = System.Math.Round(totalTime, 0);
+
+        if (min < 10)
+        {
+            minutes.text = "0" + min.ToString();
+        }
+        else
+        {
+            minutes.text = min.ToString();
+        }
+
+        if (sec < 10)
+        {
+            seconds.text = "0" + sec.ToString();
+        }
+        else
+        {
+            seconds.text = sec.ToString();
+        }
+
+    }
+}
