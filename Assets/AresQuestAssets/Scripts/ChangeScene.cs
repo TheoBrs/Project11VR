e05598cd85fdacd22d4d37a3b2fa8b02b24f145f
Justine-dt <72021396+Justine-dt@users.noreply.github.com>
2024-03-13 10:13:25 +0100

    add changeScene

---
+++ Assets/AresQuestAssets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void MenuToRule()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void RuleToGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GameToWin()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GameToLoose()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
--- Assets/AresQuestAssets/Scripts/GameManager.cs
+++ Assets/AresQuestAssets/Scripts/GameManager.cs
@@ -1,16 +1,7 @@
+using System;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-
-    public void ChangeScene()
-    {
-        SceneManager.LoadScene(1);
-    }
 
 }
